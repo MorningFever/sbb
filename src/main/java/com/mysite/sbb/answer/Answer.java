9eb44d4c075a249c6ec80bd1d2a1f610cb9788a0
MorningFever <baek6259@gmail.com>
2022-12-12 16:22:32 +0900

    중간쯤 왔음.

---
+++ src/main/java/com/mysite/sbb/answer/Answer.java
@@ -0,0 +1,22 @@
+package com.mysite.sbb.answer;
+
+import com.mysite.sbb.question.Question;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(length = 1000)
+    private String content;
+    private LocalDateTime createDate;
+    @ManyToOne
+    private Question question;
+}
